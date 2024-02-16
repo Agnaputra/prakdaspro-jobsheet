ddd8f1bde22e31444dfe67e10756763b8f1065be
nanaputra <agenaputra@gmail.com>
2024-02-16 09:49:56 +0700

    Create Gratitude_01.java

---
+++ SEMESTER 1/WEEK12/Gratitude_01.java
@@ -0,0 +1,11 @@
+package WEEK12;
+
+public class Gratitude_01 {
+
+  public static void main(String[] args) {
+    System.out.println("Thank you for being the best teacher in the world."); 
+    System.out.println("You inspired in me a love for learning and made me feel like i could ask you anything.");
+  }
+
+}
+    
